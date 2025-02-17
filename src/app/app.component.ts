d280e7e52b43ad1b96f389198bcbb1b5205b75c2
Omar Faruq <omarfaruq9723@gmail.com>
2025-02-17 18:12:54 +0600

    complete
    Only CSS can be change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { DataTableComponent } from './table/data-table/data-table.component';
 import { TabelArrayComponent } from './table/table-array/tabel-array.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [DataTableComponent,TabelArrayComponent],
+  imports: [DataTableComponent,TabelArrayComponent,HeaderComponent,FooterComponent,ContentComponent],
   // templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   template: `
-  <app-data-table></app-data-table>
-  <app-tabel-array></app-tabel-array>
+  <div class="container">
+    <app-header></app-header>
+    <app-content></app-content>
+    <app-data-table></app-data-table>
+    <app-tabel-array></app-tabel-array>
+    <app-footer></app-footer>
+  </div>
   `
 })
 export class AppComponent {
